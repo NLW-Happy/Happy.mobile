80218cc9bc71abb10a83806174edc23e6801ed87
mpgxc <mpgx5.c@gmail.com>
2020-10-17 06:36:21 -0300

    feat: call header to screens

--- src/routes/app.route.tsx
+++ src/routes/app.route.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import OrphanagesMap from '../pages/OrphanagesMap';
 import OrphanageDetails from '../pages/OrphanageDetails';
+import CreateOrphanage from './create.route';
+import Header from '../components/Header';
 
-const App: React.FC = () => {
+const AppRoute: React.FC = () => {
   const { Navigator, Screen } = createStackNavigator();
   return (
-    <Navigator headerMode="none">
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="OrphanagesMap" component={OrphanagesMap} />
-      <Screen name="OrphanageDetails" component={OrphanageDetails} />
+      <Screen
+        name="OrphanageDetails"
+        component={OrphanageDetails}
+        options={{
+          headerShown: true,
+          header: () => <Header title="Orfaato" hasCLose={false} />,
+        }}
+      />
+
+      <Screen name="CreateOrphanage" component={CreateOrphanage} />
     </Navigator>
   );
 };
 
-export default App;
+export default AppRoute;
